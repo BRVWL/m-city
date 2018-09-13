e106871dea6ea91f97ae17c708f2a89fee5257eb
Artem Semenchenko <artem.s@computools.com>
2018-09-13 17:00:44 +0300

    Added Private Route

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import SignIn from './Components/SignIn';
 import Dashboard from './Components/Admin/Dashboard';
+import PrivateRoute from './Components/AuthRoutes/PrivateRoute';
 
 class Routes extends Component {
   render() {
+    console.log('Routes props', this.props);
     return (
       <div className="Routes">
         <Layout>
           <Switch>
-            <Route exact component={Dashboard} path="/dashboard" />
+            <PrivateRoute {...this.props} exact component={Dashboard} path="/dashboard" />
             <Route exact component={SignIn} path="/sign_in" />
             <Route exact component={Home} path="/" />
           </Switch>
