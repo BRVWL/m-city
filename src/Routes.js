4848d0b0c7c11e7f594e52ec879e39a35018bc91
Artem Semenchenko <artem.s@computools.com>
2018-09-13 15:54:41 +0300

    Added nav menu

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import SignIn from './Components/SignIn';
+import Dashboard from './Components/Admin/Dashboard';
 
 class Routes extends Component {
   render() {
@@ -10,7 +11,8 @@ class Routes extends Component {
       <div className="Routes">
         <Layout>
           <Switch>
-            <Route component={SignIn} path="/sign_in" />
+            <Route exact component={Dashboard} path="/dashboard" />
+            <Route exact component={SignIn} path="/sign_in" />
             <Route exact component={Home} path="/" />
           </Switch>
         </Layout>
