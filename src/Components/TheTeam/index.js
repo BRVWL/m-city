ca9373b5ec0e73d71b799ac905f84a9ab07f9412
Artem Semenchenko <artem.s@computools.com>
2018-09-24 01:12:18 +0300

    Finished teem component

--- src/Components/TheTeam/index.js
+++ src/Components/TheTeam/index.js
@@ -5,6 +5,7 @@ import stripes from '../../Resources/images/stripes.png';
 import { reqToFirebase, firebase } from '../../firebase';
 import { firebaseLooper } from '../UI/misc';
 import { Promise } from 'core-js';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class TheTeam extends Component {
   state = {
@@ -43,11 +44,57 @@ class TheTeam extends Component {
       });
   }
 
+  showPlayersByCategory = category =>
+    this.state.players
+      ? this.state.players.map((player, i) => {
+          return player.position === category ? (
+            <Fade left key={i} delay={i * 20}>
+              <div className="item">
+                <PlayerCard
+                  number={player.number}
+                  name={player.name}
+                  lastname={player.lastname}
+                  bck={player.url}
+                />
+              </div>
+            </Fade>
+          ) : null;
+        })
+      : null;
+
   render() {
-    console.log(this.state.players);
     return (
-      <div>
-        <div>The team</div>
+      <div
+        className="the_team_container"
+        style={{ background: `url(${stripes}) repeat` }}>
+        {!this.state.loading ? (
+          <div>
+            <div className="team_category_wrapper">
+              <div className="title">Keepers</div>
+              <div className="teem_cards">{this.showPlayersByCategory('Keeper')}</div>
+            </div>
+            <div className="team_category_wrapper">
+              <div className="title">Defence</div>
+              <div className="teem_cards">{this.showPlayersByCategory('Defence')}</div>
+            </div>
+            <div className="team_category_wrapper">
+              <div className="title">Midfield</div>
+              <div className="teem_cards">{this.showPlayersByCategory('Midfield')}</div>
+            </div>
+            <div className="team_category_wrapper">
+              <div className="title">Strikers</div>
+              <div className="teem_cards">{this.showPlayersByCategory('Striker')}</div>
+            </div>
+          </div>
+        ) : (
+          <div
+            style={{
+              textAlign: 'center',
+              margin: '30px'
+            }}>
+            <CircularProgress style={{ color: '#98c6e9' }} thickness={7} />
+          </div>
+        )}
       </div>
     );
   }
